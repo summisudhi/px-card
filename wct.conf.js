410d0dfe660f2c840daa984a350398c701791aa7
Sirisha Aluri <sirisha.aluri@ge.com>
2016-04-01 10:26:43 -0700

    changes to wct conf file

--- wct.conf.js
+++ wct.conf.js
@@ -7,46 +7,46 @@ module.exports = {
         sauce: {
              "disabled" : false,
              "browsers": [
-             {
-          "browserName": "internet explorer",
-          "platform": "Windows 7",
-          "version": "10"
-        },
-        {
-          "browserName": "chrome",
-          "platform": "Windows 10",
-          "version": "44"
-        },
-         {
-          "browserName": "microsoftedge",
-          "platform": "Windows 10"
-         
-        },
-        {
-          "browserName": "internet explorer",
-          "platform": "Windows 8.1",
-          "version": "11"
-        },
-        {
-          "browserName": "internet explorer",
-          "platform": "Windows 10",
-          "version": "11"
-        },
-        {
-          "browserName": "internet explorer",
-          "platform": "Windows 8",
-          "version": "10"
-        },
-         {
-          "browserName": "firefox",
-          "platform": "Windows 8.1",
-          "version": "44"
-        },
-         {
-          "browserName": "chrome",
-          "platform": "Windows 8.1",
-          "version": "48"
-        },
+             // {
+//           "browserName": "internet explorer",
+//           "platform": "Windows 7",
+//           "version": "10"
+//         },
+//         {
+//           "browserName": "chrome",
+//           "platform": "Windows 10",
+//           "version": "44"
+//         },
+//          {
+//           "browserName": "microsoftedge",
+//           "platform": "Windows 10"
+//          
+//         },
+//         {
+//           "browserName": "internet explorer",
+//           "platform": "Windows 8.1",
+//           "version": "11"
+//         },
+//         {
+//           "browserName": "internet explorer",
+//           "platform": "Windows 10",
+//           "version": "11"
+//         },
+//         {
+//           "browserName": "internet explorer",
+//           "platform": "Windows 8",
+//           "version": "10"
+//         },
+//          {
+//           "browserName": "firefox",
+//           "platform": "Windows 8.1",
+//           "version": "44"
+//         },
+//          {
+//           "browserName": "chrome",
+//           "platform": "Windows 8.1",
+//           "version": "48"
+//         },
          {
           "browserName": "safari",
           "platform": "OS X 10.10",
@@ -55,29 +55,34 @@ module.exports = {
         {
           "browserName": "chrome",
           "platform": "OS X 10.10",
-          "version": "48"
-        },
-         
-        
-         {
-          "browserName": "chrome",
-          "platform": "OS X 10.11",
-          "version": "48"
-        },
+          "version": "49"
+        } ,
+        {
+          "browserName": "firefox",
+          "platform": "OS X 10.10",
+          "version": "44"
+        } 
+//          
+//         
+//          {
+//           "browserName": "chrome",
+//           "platform": "OS X 10.11",
+//           "version": "48"
+//         },
              
            
-{
-          "browserName": "safari",
-          "platform": "OS X 10.11",
-          "version": "9"
-        },
+// {
+//           "browserName": "safari",
+//           "platform": "OS X 10.11",
+//           "version": "9"
+//         },
              
-         {
-          "browserName": "iphone",
-          "platform": "OS X 10.10",
-          "version": "9.2",
-          "deviceName": "iPad Retina",
-          "deviceOrientation": "portrait"
+       //   {
+//           "browserName": "iphone",
+//           "platform": "OS X 10.10",
+//           "version": "9.2",
+//           "deviceName": "iPad Retina",
+//           "deviceOrientation": "portrait"
           
         }// ,
 //         
@@ -97,5 +102,7 @@ module.exports = {
             
             
         }
-    }
+    } ,
+   
+    suites:      ['test/*.html']
 };
