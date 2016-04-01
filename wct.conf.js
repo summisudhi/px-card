792870586197b6158b31be438821f2dbb78faa9c
Sirisha Aluri <sirisha.aluri@ge.com>
2016-04-01 10:37:19 -0700

    added set of browsers only to wct file

--- wct.conf.js
+++ wct.conf.js
@@ -5,48 +5,9 @@ module.exports = {
             browsers: ['chrome', 'firefox']
         },
         sauce: {
-             "disabled" : false,
+             "disabled" : true,
              "browsers": [
-             // {
-//           "browserName": "internet explorer",
-//           "platform": "Windows 7",
-//           "version": "10"
-//         },
-//         {
-//           "browserName": "chrome",
-//           "platform": "Windows 10",
-//           "version": "44"
-//         },
-//          {
-//           "browserName": "microsoftedge",
-//           "platform": "Windows 10"
-//          
-//         },
-//         {
-//           "browserName": "internet explorer",
-//           "platform": "Windows 8.1",
-//           "version": "11"
-//         },
-//         {
-//           "browserName": "internet explorer",
-//           "platform": "Windows 10",
-//           "version": "11"
-//         },
-//         {
-//           "browserName": "internet explorer",
-//           "platform": "Windows 8",
-//           "version": "10"
-//         },
-//          {
-//           "browserName": "firefox",
-//           "platform": "Windows 8.1",
-//           "version": "44"
-//         },
-//          {
-//           "browserName": "chrome",
-//           "platform": "Windows 8.1",
-//           "version": "48"
-//         },
+             
          {
           "browserName": "safari",
           "platform": "OS X 10.10",
@@ -62,38 +23,7 @@ module.exports = {
           "platform": "OS X 10.10",
           "version": "44"
         } 
-//          
-//         
-//          {
-//           "browserName": "chrome",
-//           "platform": "OS X 10.11",
-//           "version": "48"
-//         },
-             
-           
-// {
-//           "browserName": "safari",
-//           "platform": "OS X 10.11",
-//           "version": "9"
-//         },
-             
-       //   {
-//           "browserName": "iphone",
-//           "platform": "OS X 10.10",
-//           "version": "9.2",
-//           "deviceName": "iPad Retina",
-//           "deviceOrientation": "portrait"
-          
-        }// ,
-//         
-//         {
-//           "browserName": "iphone",
-//           "platform": "OS X 10.10",
-//           "version": "9.2",
-//           "deviceName": "iPad 2",
-//           "deviceOrientation": "portrait"
-//           
-//         }
+
         
         
       ]
